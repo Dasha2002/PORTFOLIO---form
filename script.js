59d36ac5be2a3600f9ea168d7f13b3760f12e1fd
Dasha2002 <kosolapova.dasha2018@yandex.ru>
2025-10-04 11:44:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+const age = prompt("Сколько тебе лет?", 18) 
+
+console.log(`Возраст: ${age}`)
+if (age >= 18) {
+    alert('Доступ разрешен')
+} else {
+    alert('Доступ запрещен')
+}
+
+const two = confirm('Готовы начинать?')
+*/
